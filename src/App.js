3582b0c58a676a4f98cd5a67349f631b4e2e7cb9
ddanny16 <102356223+ddanny16@users.noreply.github.com>
2025-05-01 21:27:24 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  const token = localStorage.getItem("token");
-
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={token ? <Navigate to="/dashboard" /> : <Login />}
-        />
-        <Route
-          path="/dashboard"
-          element={token ? <Dashboard /> : <Navigate to="/" />}
-        />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
